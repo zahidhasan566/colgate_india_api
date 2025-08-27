815d9c3d48d7001e82a7f3f1605acd58efe840ac
Md Zahid Hasan <jahid1234.jh@gmail.com>
2025-08-27 17:42:51 +0600

    jh

--- config/sftp.php
+++ config/sftp.php
@@ -5,5 +5,5 @@ return [
     'port' => 8854,
     'username' => 'aci-systems',
     'password' => base64_decode('SDNTdG9EcnQ2wqNiJj09NWIxJWxyPFx1WEY='),
-    'remote_path' => '/aci-systems/Test Folder',
+    'remote_path' => '/aci-systems/aci_prod',
 ];
